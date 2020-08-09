ec54b8d1c981b7106c1d4070bd1e974831218f41
Roman Yakovlev <tensegrity666@eicloud.com>
2020-08-09 23:38:05 +0300

    fix: port

--- src/utils/xeno-canto-api/index.js
+++ src/utils/xeno-canto-api/index.js
@@ -2,9 +2,11 @@ class XenoCantoApi {
   apiBase = 'https://www.xeno-canto.org/api/2/recordings?query=goose';
 
   async getData(req) {
+    const port = process.env.PORT || 8000;
+
     this.req = req;
 
-    const res = await fetch(`http://localhost:8000/query/${this.req}`);
+    const res = await fetch(`http://localhost:${port}/query/${this.req}`);
     const data = await res.json();
 
     return data;
