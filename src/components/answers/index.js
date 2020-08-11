a8409657dbb82b9d9b2c80179f6ff626c5a985ad
Roman Yakovlev <tensegrity666@eicloud.com>
2020-08-11 18:03:29 +0300

    feat: add soundplayer, styles etc

--- src/components/answers/index.js
+++ src/components/answers/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-// import styles from './index.module.css';
+import styles from './index.module.css';
 
 const Answers = () => {
+  const { answers } = styles;
   return (
-    <ul className="list-group col-12 col-md-6">
+    <ul className={`list-group col-12 col-md-6 ${answers}`}>
       <li className="list-group-item d-flex justify-content-between align-items-center">
         <button type="button" className="btn btn-info">
           Button
