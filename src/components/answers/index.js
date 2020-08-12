056d8d6468f9c1c36238b81e448f46aac488a856
Roman Yakovlev <tensegrity666@eicloud.com>
2020-08-12 23:36:08 +0300

    feat: add photos

--- src/components/answers/index.js
+++ src/components/answers/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 
 import styles from './index.module.css';
 
+import { getArrayOfNames } from '../../data';
+
 const Answers = () => {
-  const { answers } = styles;
-  return (
-    <ul className={`list-group col-12 col-md-6 ${answers}`}>
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        <button type="button" className="btn btn-info">
-          Button
+  const { answers, transparentize, button } = styles;
+  const names = getArrayOfNames(2);
+
+  const buttons = names.map((name) => {
+    return (
+      <li
+        key={nanoid()}
+        className={`list-group-item d-flex justify-content-between align-items-center ${transparentize}`}>
+        <button type="button" className={`btn btn-info ${button}`}>
+          {name}
         </button>
       </li>
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        <button type="button" className="btn btn-primary">
-          Dapibus ac facilisis in
-        </button>
-      </li>
-      <li className="list-group-item d-flex justify-content-between align-items-center">
-        <button type="button" className="btn btn-primary">
-          Morbi leo risus
-        </button>
-      </li>
-    </ul>
-  );
+    );
+  });
+
+  return <ul className={`list-group col-12 col-md-3 ${answers}`}>{buttons}</ul>;
 };
 
 export default Answers;
--- src/components/info/index.js
+++ src/components/info/index.js
@@ -5,42 +5,75 @@ import React, { Component } from 'react';
 
 import XenoCantoApi from '../../utils/xeno-canto-api';
 
+import { getInfo } from '../../data';
+
 import styles from './index.module.css';
 
 class Info extends Component {
   xenoCantoApi = new XenoCantoApi();
 
   state = {
-    data: {},
+    name: '',
+    latinName: '',
+    audioURL: '',
+    link: '',
+    description: '',
   };
 
   componentDidMount() {
     this.updateAudio();
-    // this.updateImage()
+    this.updateInfo(0);
   }
 
-  onDataLoaded = (data) => {
-    this.setState({ data });
-  };
-
   updateAudio() {
-    this.xenoCantoApi.getData('goose').then(this.onDataLoaded);
+    this.xenoCantoApi.getData('goose').then((audio) =>
+      this.setState({
+        name: audio.name,
+        latinName: audio.latinName,
+        audioURL: audio.audioURL,
+      })
+    );
+  }
+
+  updateInfo(id) {
+    const info = getInfo(id, 5);
+    const { link, description } = info;
+    this.setState({
+      link,
+      description,
+    });
   }
 
   render() {
+    const { name, latinName, audioURL, description, link } = this.state;
     const {
-      data: { name, latinName, audioURL, cardText, imageURL },
-    } = this.state;
-    const { infoBlock, soundPlayer } = styles;
+      infoBlock,
+      soundPlayer,
+      image,
+      container,
+      cardInner,
+      containerInner,
+      paragraph,
+    } = styles;
 
     return (
-      <div className={`card border-secondary col-12 col-md-6 ${infoBlock}`}>
-        <img src={imageURL} alt={name} />
-        <div className="card-header">{name}</div>
-        <div className="card-body">
-          <h4 className="card-title">{latinName}</h4>
-          <audio className={soundPlayer} src={audioURL} controls />
-          <p className="card-text">{cardText}</p>
+      <div
+        className={`card border-secondary col-12 col-sm-12 col-md-9 ${infoBlock}`}>
+        <div className={container}>
+          <img
+            className={`${image}`}
+            src={`${process.env.PUBLIC_URL}${link}`}
+            alt={name}
+            width="400"
+          />
+          <div className={`card-body ${cardInner}`}>
+            <h3 className="card-header">{name}</h3>
+            <div className={containerInner}>
+              <h5 className="card-title">{latinName}</h5>
+              <audio className={soundPlayer} src={audioURL} controls />
+              <p className={`card-text ${paragraph}`}>{description}</p>
+            </div>
+          </div>
         </div>
       </div>
     );
